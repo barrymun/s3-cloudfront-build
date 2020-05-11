7d6fe7dacdccca14beaa7dc691f6c23423d3fd4a
Neil <barrymun@tcd.ie>
2020-05-11 18:53:52 +0100

    improved script functionality (see comments)

--- main.sh
+++ main.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# *** avoid s3cmd
+# ref: https://stackoverflow.com/a/10406356
+# and: https://petersmith.org/blog/2017/05/31/mime-types-on-asw-s3/
 if [ -z "$1" ]
   then
     echo "s3 bucket name cannot be an empty string"
@@ -6,12 +9,12 @@ if [ -z "$1" ]
 fi
 if [ -z "$2" ]
   then
-    echo "distribution id cannot be an empty string"
+    echo "cloudfront distribution id cannot be an empty string"
     exit 1
 fi
-s3cmd del "s3://$1" --recursive --force
 npm run build
 rm build/.DS_Store
-s3cmd sync ./build/ "s3://$1"
+aws s3 rm --recursive "s3://$1"  # delete the contents of the bucket
+aws s3 sync ./build/ "s3://$1"
 aws cloudfront create-invalidation --distribution-id $2 --paths "/*"
 rm -rf build
